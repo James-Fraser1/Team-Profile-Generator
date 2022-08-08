eb6e60737adf8b6a8a0a7f8cde577811714e7015
James-Fraser1 <jdfraser2022@gmail.com>
2022-08-08 11:15:28 -0400

    file path created, functions added, javascript linked

--- index.js
+++ index.js
@@ -1,56 +1,144 @@
 const inquirer = require('inquirer');
 const fs = require("fs");
-const TeamProfile = require('./src/generator');
+const teamProfile = require('./src/generator');
 
 const Engineer = require("./lib/Engineer")
 const Intern = require("./lib/Intern")
 const Manager = require("./lib/Manager")
 
-const manArray = [];
+const teamArray = [];
 
 const managerQuestions = [
-    {
-        type: 'input',
-        message: "What is your team manager's name?",
-        name: 'managerName',
-        // validate: (value) => { if (value) { return true } else { return 'I need a value to continue' } }
-    },
-    {
-        type: 'input',
-        message: "What is your team manager's employee ID?",
-        name: 'managerID',
-        validate: (value) => { if (value) { return true } else { return 'I need a value to continue' } }
-    },
-    {
-        type: 'input',
-        message: "What is your team manager's email address?",
-        name: 'managerEmail',
-        validate: (value) => { if (value) { return true } else { return 'I need a value to continue' } }
-    },
-    {
-        type: 'input',
-        message: "What is your team manager's office number?",
-        name: 'managerNumber',
-        validate: (value) => { if (value) { return true } else { return 'I need a value to continue' } }
-    },
-];
-
-console.log(managerQuestions)
-
-function runManager() {
-    return inquirer.prompt(managerQuestions)
-        .then((data) => {
-            const newManager = new Manager(data)
-            manArray.push(newManager)
+    inquirer.prompt(
+        {
+            type: 'input',
+            message: "What is your team manager's name?",
+            name: 'managerName',
+        },
+        {
+            type: 'input',
+            message: "What is your team manager's employee ID?",
+            name: 'managerID',
+        },
+        {
+            type: 'input',
+            message: "What is your team manager's email address?",
+            name: 'managerEmail',
+        },
+        {
+            type: 'input',
+            message: "What is your team manager's office number?",
+            name: 'managerNumber',
+        },
+    )
+        .then((managerQuestions) => {
+            const newManager = new Manager(managerQuestions)
+            teamArray.push(newManager)
+            selectTeam();
             console.log(manArray)
         })
         .catch((error) => {
             console.log(error)
         })
+];
+
+const selectTeam = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: "What license did you use?",
+            name: 'license',
+            choices: ['Engineer', 'Intern', 'Complete Team'],
+        },
+    ])
+        .then(userChoice => {
+            switch (userChoice.profile) {
+                case 'Engineer':
+                    engineerQuestions();
+                    break;
+
+                case 'Intern':
+                    internQuestions();
+
+                    break;
+                default: generateChoice();
+            }
+        })
+};
+
+const engineerQuestions = [
+    inquirer.prompt(
+        {
+            type: 'input',
+            message: "What is your engineer's name?",
+            name: 'engineerName',
+        },
+        {
+            type: 'input',
+            message: "What is your engineer's employee ID?",
+            name: 'engineerID',
+        },
+        {
+            type: 'input',
+            message: "What is your engineer's email address?",
+            name: 'engineerEmail',
+        },
+        {
+            type: 'input',
+            message: "What is your engineer's office number?",
+            name: 'engineerNumber',
+        },
+    )
+        .then((engineerQuestions) => {
+            const newEngineer = new Engineer(engineerQuestions)
+            teamArray.push(newEngineer)
+            selectTeam();
+            console.log(engineerArray)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+];
+
+const internQuestions = [
+    inquirer.prompt(
+        {
+            type: 'input',
+            message: "What is your Intern's name?",
+            name: 'internName',
+        },
+        {
+            type: 'input',
+            message: "What is your intern's employee ID?",
+            name: 'internID',
+        },
+        {
+            type: 'input',
+            message: "What is your Intern's email address?",
+            name: 'internEmail',
+        },
+        {
+            type: 'input',
+            message: "What is your Intern's office number?",
+            name: 'internNumber',
+        },
+    )
+        .then((internQuestions) => {
+            const newIntern = new Intern(internQuestions)
+            teamArray.push(newIntern)
+            selectTeam();
+            console.log(internArray)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+];
+
+const generateChoice = () => {
+    fs.writeFileSync('./dist/index.html', teamProfile(teamArray))
 }
 
-runManager();
-
+managerQuestions();
 
 // Start Application
 // Ask Questions from the Manager
@@ -59,13 +147,6 @@ runManager();
 // On click "Intern", ask questions about Intern
 // On click "Engineer", ask questions about Engineer
 // Generate an HTML
-
-
-const prompt = {
-
-}
-
-
 // {
 //     type: 'list',
 //     message: "What license did you use?",
@@ -73,11 +154,4 @@ const prompt = {
 //     choices: ['MIT', 'Apache', 'BSD'],
 //     validate: (value) => { if (value) { return true } else { return 'I need a value to continue' } }
 // },
-
-
-
-
-
-
-
-new TeamProfile().initializeTeamProfile();
+// new TeamProfile().initializeTeamProfile();
